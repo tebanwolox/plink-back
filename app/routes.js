2e60185c780bce0a7191a280e74239ed16f17ad8
tebanwolox <esteban.bedoya@wolox.co>
2019-11-05 19:17:16 -0500

    add sessions route

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,12 @@
 const { checkSchema } = require('express-validator');
 
 const { healthCheck } = require('./controllers/healthCheck');
-const { createUser } = require('./controllers/user');
-const { signUpSchema } = require('./schemas/user');
-const { signUpMiddleware } = require('./middlewares/user');
+const { createUser, logging } = require('./controllers/user');
+const { signUpSchema, signInSchema } = require('./schemas/user');
+const { signUpMiddleware, signInMiddleware } = require('./middlewares/user');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.post('/users', checkSchema(signUpSchema), signUpMiddleware, createUser);
+  app.post('/users/sessions', checkSchema(signInSchema), signInMiddleware, logging);
 };
