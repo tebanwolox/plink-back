9fce21851bb2ee57b950f5847102430919e41e58
tebanwolox <esteban.bedoya@wolox.co>
2019-11-06 12:01:06 -0500

    fix validation result

--- app/helpers/crypt.js
+++ app/helpers/crypt.js
@@ -1,9 +1,11 @@
 const bcrypt = require('bcrypt');
+
 const logger = require('../logger');
 const errors = require('../errors');
+const { saltRounds } = require('../../config').common.bcrypt;
 
 exports.hashingPassword = password =>
-  bcrypt.hash(password, parseInt(process.env.CRYPT_SALT_ROUNDS)).catch(err => {
+  bcrypt.hash(password, parseInt(saltRounds)).catch(err => {
     logger.error(err.message);
     return Promise.reject(errors.createUserError('Error trying to crypt the password'));
   });
--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,9 @@
-const { checkSchema } = require('express-validator');
-
 const { healthCheck } = require('./controllers/healthCheck');
 const { createUser } = require('./controllers/user');
 const { signUpSchema } = require('./schemas/user');
-const { signUpMiddleware } = require('./middlewares/user');
+const { validateSchema } = require('./middlewares/user');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', checkSchema(signUpSchema), signUpMiddleware, createUser);
+  app.post('/users', validateSchema(signUpSchema), createUser);
 };
