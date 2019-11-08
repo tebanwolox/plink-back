6c9b9f8b097a0fce7f277425da3e85847b3a1ac8
tebanwolox <esteban.bedoya@wolox.co>
2019-11-07 20:53:17 -0500

    correct payload

--- app/helpers/jwt.js
+++ app/helpers/jwt.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const { secret, expirationToken } = require('../../config').common.session;
 
-exports.getToken = payload => jwt.sign(payload, secret, { expiresIn: expirationToken });
+exports.getToken = ({ id, firstName, lastName, currency }) =>
+  jwt.sign({ id, firstName, lastName, currency }, secret, { expiresIn: expirationToken });
 
 exports.verifiedToken = token => jwt.verify(token, secret);
