c479ba3ff1908979ea69e8b75b80bce21636a674
tebanwolox <esteban.bedoya@wolox.co>
2019-11-12 10:35:44 -0500

    delete nock file

--- test/mock/nock.js
+++
@@ -1,8 +0,0 @@
-const nock = require('nock');
-
-const { braveCoinEnpoint } = require('../../config').common.braveCoin;
-const { testTickerResponse } = require('./coin');
-
-exports.successTicker = nock(braveCoinEnpoint)
-  .get(/ticker\?[^/]+$/)
-  .reply(200, testTickerResponse);
